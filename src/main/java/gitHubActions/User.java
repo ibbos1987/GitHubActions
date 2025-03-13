c01b3c072b01f576b5cddb1f63f8d507031b0d06
ibbos1987 <ibrahim.albatal@gmail.com>
2025-03-13 09:26:57 +0100

    Initial commit

---
+++ src/main/java/gitHubActions/User.java
@@ -0,0 +1,24 @@
+package gitHubActions;
+
+public class User {
+    private String userName;
+    private String passWord;
+
+    public User(String userName, String passWord){
+        this.userName= userName;
+        this.passWord = passWord;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void changeUserName(String userName) {
+        this.userName=userName;
+    }
+}
---
+++ src/test/java/gitHubActions/UserTest.java
@@ -0,0 +1,24 @@
+package gitHubActions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void testUser() {
+        User user = new User("Arne", "Alligator");
+
+        assertEquals("Arne", user.getUserName());
+        assertEquals("Alligator", user.getPassWord());
+    }
+
+    @Test
+    public void testChangeUserName(){
+        User user=new User("Arne","Alligator");
+
+        user.changeUserName("Arnold");
+        assertEquals("Arnold",user.getUserName());
+    }
+}
